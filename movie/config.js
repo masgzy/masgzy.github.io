6fa443afdb2465a932824f10682c696b1ec92553
masgzy <aaallsky@outlook.com>
2025-05-01 15:24:41 +0800

    none

--- movie/config.js
+++ movie/config.js
@@ -1,8 +1,12 @@
 const config = {
-    movies: [
-        {
-            name: "心灵奇旅",
-            url: "https://ppvod01.blbtgg.com/splitOut/20240716/392023/V2024071601481480491392023/index.m3u8?auth_key=1746082748-74e1d249dae342f0969f4615c3401d86-0-18af76a25184c77e264ad797a4ae27ed"
-        }
-    ]
+  // 全局 API 地址（需替换为您的解析服务）
+  apiURL: "https://jx.xnflv.cc/?url=",
+
+  // 电影列表（仅需名称和视频 ID）
+  movies: [
+    {
+      name: "心灵奇旅",
+      videoId: "https://www.iqiyi.com/v_srhoj3lh48.html"
+    }
+  ]
 };
