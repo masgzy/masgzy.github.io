6aa8b686a0e7e6c54701c82438b2fdaf194913c6
masgzy <aaallsky@outlook.com>
2025-05-01 14:10:35 +0800

    none

--- res/config.js
+++ res/config.js
@@ -1,22 +1,17 @@
 const cards = [
   {
-    title: "项目 A",
-    description: "这是项目A的介绍。",
-    imageUrl: "image_a.jpg",
-    link: "https://example.com/project-a",
-    iconUrl: "icon_a.png"
-  },
-  {
-    title: "项目 B",
-    description: "这是项目B的介绍。",
-    imageUrl: "image_b.jpg",
-    link: "https://example.com/project-b",
-    iconUrl: "icon_b.png"
-  },
-  {
-    title: "项目 C",
-    description: "这是项目C的介绍。",
-    link: "https://example.com/project-c",
-    iconUrl: "icon_c.png"
+    title: "下载页",
+    description: "只是一些文件...",
+    link: "/download",
+    iconUrl: "img/download.png"
   }
 ];
+
+/*{
+    title: "下载页",
+    description: "只是一些文件...",
+    imageUrl: "image",
+    link: "https://example.com/project-a",
+    iconUrl: "icon_a.png"
+  }
+  */
